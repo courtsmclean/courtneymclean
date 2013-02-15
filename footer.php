793132b207e7ccf292c0a42e6704458139aff5fc
Courtney Mclean <courtsmclean@gmail.com>
2013-02-15 16:39:07 +0000

    Initial files commit
    commit all files for the first time - ready to test to ensure that it
    works correctly - JUST NEEDS AN INDEX.PHP

---
+++ footer.php
@@ -0,0 +1,33 @@
+</div><!-- #main -->
+	
+<!-- Footer -->
+	<footer class="dark-brown-bg">
+		<div class="container">
+			<h1>I would love to hear from you,<br /> so drop me a line</h1>
+				<a href="" class="button poached_ivory" title="">Get in Touch</a>
+			<nav role="navigation">
+				<ul class="social-links">
+				    <li id="twitter"><a href="#1"></a></li>
+				    <li id="linkedin"><a href="#2"></a></li>
+				    <li id="behance"><a href="#3"></a></li>
+				</ul>	
+			</nav>
+		
+						
+	<!-- end footer container -->
+		</div>
+	</footer>
+
+
+<!-- JavaScript at the bottom for fast page loading -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<script src="js/selectivizr-min.js"></script>
+	<![endif]-->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!-- HTML5 Mobile Boilerplate -->
+<!--[if IEMobile 7]><html class="no-js iem7"><![endif]-->
+<!--[if (gt IEMobile 7)|!(IEMobile)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<html <?php language_attributes(); ?>>
+
+<!-- HTML5 Boilerplate -->
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"><!--<![endif]-->
+
+<head>
+
+	<meta charset="utf-8">
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<!-- Meta descriptions -->
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="description" content="This is the home of graphic designer Courtney Mclean.">
+	<meta name="keywords" content="graphic design, front-end, developer, responsive, grid, system, web design">
+	<meta name="author" content="www.courtneymclean.co.uk">
+	<meta http-equiv="cleartype" content="on">
+	<link rel="shortcut icon" href="<?php echo THEME_DIR; ?>/path/favicon.ico" /> 
+	
+	<!-- Responsive and mobile friendly stuff -->
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+
+	<!-- Title -->
+    <title><?php wp_title(); ?> - <?php bloginfo( 'name' ); ?></title>  
+
+	<!-- Stylesheets -->
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+
+	<!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements and feature detects -->
+	<script src="js/modernizr-2.5.3-min.js"></script>
+      
+    <!--=== WP_HEAD() ===-->  
+    <?php wp_head(); ?>  
+	
+	</head>
+	
+	
+	<body <?php body_class(); ?>>  
+	
+	
+	<!-- Header -->
+	<header>
+		<div class="container">
+		
+		<!-- logo -->
+			<div class="three col">
+				<a href="<?php echo home_url(); ?>" title=""><img src="<?php bloginfo('template_directory'); ?>/images/courtneymclean_logomark.png" class="scale-with-grid" title="" alt="" /></a>
+				<a href="<?php echo home_url(); ?>" title=""><img src="<?php bloginfo('template_directory'); ?>/images/courtneymclean_logotype.png" class="scale-with-grid right" title="" alt="" /></a>
+			</div>
+
+		<!-- menu -->
+			<div class="eight col right">
+				<nav class="nav-collapse" role="navigation">
+					<ul>
+					<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) ); ?>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	
+	<!-- end header -->
+	</header>
+	
+	<div id="main">
