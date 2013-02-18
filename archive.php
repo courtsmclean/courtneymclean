9698c734f5ae6be0d7e0fa068c2e49d3a5278499
Courtney Mclean <courtsmclean@gmail.com>
2013-02-18 14:06:47 +0000

    Revert "updated archive - may need to roll back"
    This reverts commit 50c041b301f6dcf957c8a8171d8220c5e28c979a.

--- archive.php
+++ archive.php
@@ -1,4 +1,8 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Archives
+*/
+get_header(); ?>
 
 
 <!-- Introduction -->
@@ -22,31 +26,30 @@
 
 <!-- Latest Blog Post -->
 	<section id="latest-post">
-		
-		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-          <?php /* If this is a category archive */ if (is_category()) { ?>
-                <h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category:</h2>
-          <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-                <h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
-          <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-                <h2>Archive for <?php the_time('F jS, Y'); ?>:</h2>
-          <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-                <h2>Archive for <?php the_time('F, Y'); ?>:</h2>
-          <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-                <h2>Archive for <?php the_time('Y'); ?>:</h2>
-          <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-                <h2>Author Archive</h2>
-          <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-                <h2>Blog Archives</h2>
-        <?php } ?>
-        
-   
-     <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
-
-        
-        
 		<div class="container">
 
+	<!-- spacer --><div class="three col"></div>
+
+			<div class="seven col">
+				<?php the_post(); ?>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+				
+				<?php get_search_form(); ?>
+				
+				<h2>Archives by Month:</h2>
+				<ul>
+					<?php wp_get_archives('type=monthly'); ?>
+				</ul>
+				
+				<h2>Archives by Subject:</h2>
+				<ul>
+					 <?php wp_list_categories(); ?>
+				</ul>
+			</div>
+
+
+   
+     <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 
 		<!-- date & category -->
 			<div class="three col">
